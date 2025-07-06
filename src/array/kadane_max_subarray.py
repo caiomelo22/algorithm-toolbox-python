8e2a4a8fb3205f364904e479395ef2aa2cfd058b
Caio Graça Melo <caio.g.melo@hotmail.com>
2025-07-06 16:52:03 -0300

    Kadane's algorithm

---
+++ src/array/kadane_max_subarray.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        curr_sum = 0
+        max_sum = -float('inf')
+
+        for i in range(len(nums)):
+            curr_sum = max(nums[i], curr_sum + nums[i])
+            max_sum = max(max_sum, curr_sum)
+
+        return max_sum
